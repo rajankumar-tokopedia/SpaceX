49767f569694e62476dcd27f6af76ec3e0458123
rajankumar-tokopedia <rajan.kumar@tokopedia.com>
2020-09-28 01:25:04 +0530

    removed log

--- src/index.js
+++ src/index.js
@@ -9,13 +9,9 @@ const server = http.createServer(app);
 
 let currentApp = app;
 let port = process.env.PORT || 3000
-console.log(process.env.PORT, JSON.stringify(process.env));
 var jsonenv = JSON.stringify(process.env);
 var obj = JSON.parse(jsonenv);
-console.log(obj)
-console.log(port, obj.PORT)
 port = obj.PORT || 3000
-console.log(obj.PORT, JSON.stringify(obj));
 server
     .listen(port, () => {
       console.log('🚀 started %d', port);
